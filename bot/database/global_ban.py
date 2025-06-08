1e9bc2571c7eb82ab9d8e6c8bc8d25bb53968f8c
Elinsrc <bitkurky@gmail.com>
2025-06-08 16:49:17 +0500

    Remove unused code

--- bot/database/global_ban.py
+++ bot/database/global_ban.py
@@ -17,18 +17,6 @@ async def is_user_banned(user_id: int) -> bool:
     async with conn.execute('SELECT user_id FROM banned_users WHERE user_id = ?', (user_id,)) as cursor:
         return await cursor.fetchone() is not None
 
-async def group_add_to_ban(group_id: int):
-    await conn.execute('INSERT INTO banned_groups (group_id) VALUES (?)', (group_id,))
-    await conn.commit()
-
-async def group_remove_from_ban(group_id: int):
-    await conn.execute('DELETE FROM banned_groups WHERE group_id = ?', (group_id,))
-    await conn.commit()
-
-async def is_group_banned(group_id: int) -> bool:
-    async with conn.execute('SELECT group_id FROM banned_groups WHERE group_id = ?', (group_id,)) as cursor:
-        return await cursor.fetchone() is not None
-
 def check_ban(func):
     async def wrapper(client, message, *args, **kwargs):
         if message.from_user:
